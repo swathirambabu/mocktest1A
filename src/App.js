106ea533cbc42236ba0f56abb7cfa61904eac2b9
swathi <uttaralaswathi1998@gmail.com>
2023-10-27 09:45:21 +0000

    1A

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import TextInput from './components/TextInput'
+import './App.css'
+
+// Replace your code here
+const App = () => <TextInput />
+export default App
---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5d0fe;
+`
+export const CardContainer = styled.div`
+padding:30px;
+height:200px;
+background-color:white;
+display:flex;
+align-items:center;
+flex-direction:column;
+justify-content:center;
+text-align:centerl
+border-radius:10px;
+`
+
+export const Heading = styled.h1`
+  font-size: 20px;
+  color: #000000;
+  font-family: roboto;
+`
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+export const Paragraph = styled.p`
+  font-size: 15px;
+  font-family: roboto;
+`
+export const Input = styled.input`
+  height: 30px;
+  width: 250px;
+  outline: none;
+  margin-right: 10px;
+  color: #323f4b;
+`
+export const Button = styled.button`
+  width: 55px;
+  height: 30px;
+  color: white;
+  border-radius: 5px;
+  background-color: #d946ef;
+  font-size: 13px;
+`
